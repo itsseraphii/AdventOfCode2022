85433d31b89ed58c005006dc4d8f0e72be3c0378
Jordan Gagnon <jordan.gagnon@paradocs.ca>
2022-12-10 11:45:41 -0500

    Day 10: Cathode-Ray Tube
    Printing part 2 but no good answer, sadge. Also refactor

--- days/day10.py
+++ days/day10.py
@@ -4,32 +4,44 @@ from utils.aoc_utils import AOCDay, day
 @day(10)
 class Day10(AOCDay):
     def common(self):
+        self.cycle = 1
+        self.register = 1
         self.instructions = list(map(lambda x: (x[0:4], int(x[5:]) if x[3] != 'p' else 0), self.inputData))
-        return 0
-
-    def part1(self):
-        register = 1
-        # List of tuples (cycle, register)
         self.signalStrength = []
-        cycle = 1
+        self.CRT = []
 
         for instruction, value in self.instructions:
             if instruction == 'noop':
-                cycle += 1
+                self.computeCycle()
             elif instruction == 'addx':
-                cycle += 1
-                self.logSignalStrength(cycle, register)
-                cycle += 1
-                register += value
+                self.computeCycle()
+                self.computeCycle()
+                self.register += value
 
-            self.logSignalStrength(cycle, register)
+        return 0
 
-
+    def part1(self):
         return sum(map(lambda x: x[0]*x[1], self.signalStrength))
 
-    def logSignalStrength(self, cycle: int, register: int):
-        if (((cycle - 20) % 40 ) == 0):
-            self.signalStrength.append((cycle, register))
-
     def part2(self):
+        # Split CRT into lines
+        screen = []
+        for i in range(0, len(self.CRT), 40):
+            screen.append(self.CRT[i:i+40])
+        
+        for line in screen:
+            print(''.join(line))
+
         return 0
+
+    def computeCycle(self):
+        sprite = (self.register - 1, self.register, self.register + 1)
+        if self.cycle % 40 in sprite:
+            self.CRT.append('#')
+        else:
+            self.CRT.append('.')
+
+        if (((self.cycle - 20) % 40 ) == 0):
+            self.signalStrength.append((self.cycle, self.register))
+
+        self.cycle += 1
